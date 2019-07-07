2867bb15d6b27c06410e659af22ab38ec7037c1d
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2019-07-07 20:32:13 +0530

    Add Files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Personal-Website
+The subsections will be 
+1. Personal
+2. Products
+3. Work Experience
+4. Achievements
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/new')
+def new_page():
+    return render_template('new.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
