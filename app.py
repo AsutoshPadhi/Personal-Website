a5979385f83b1c5d35909095806380c2c35609da
Asutosh Padhi <2015asutosh.padhi@ves.ac.in>
2019-07-07 20:59:10 +0530

    Static-ify

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+# @app.route('/test')
+# def test():
+#     return render_template('test.html')
 
-@app.route('/new')
-def new_page():
-    return render_template('new.html')
+# @app.route('/new')
+# def new_page():
+#     return render_template('new.html')
 
-@app.route('/resume')
-def resume():
-    return render_template('resume.html')
+# @app.route('/resume')
+# def resume():
+#     return render_template('resume.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
